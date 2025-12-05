88b0bdbadc02dcbe82fbb65c5bcf74dd89b6637d
Fardin Chowdhury <cfardin51@gmail.com>
2025-12-05 12:31:01 +0600

    Add sortColors function to sort an array of colors
    Implemented a solution to sort colors using the sort function.

---
+++ Sort_colors.cpp
@@ -0,0 +1,9 @@
+// Sort colors
+// https://leetcode.com/problems/sort-colors/description/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+    }
+};
